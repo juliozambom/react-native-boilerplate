29a47bc4651b8fa5486dd6ce98fdf7811e9c09a6
juliozambom <work.juliozambom@gmail.com>
2025-01-11 00:37:43 -0300

    feat: start home page with project tech stack

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView className="flex-1">
+      <View className="px-8 pt-8">
+        <View className="header">
+          <Text className="text-3xl font-bold">Hello World!</Text>
+          <Text className="text-xl">
+            This project is a simple boilerplate for a React Native app.
+          </Text>
+        </View>
+
+        <View className="body mt-12">
+          <Text className="text-xl font-bold">
+            This project has the following tech stack:
+          </Text>
+
+          <View className="tech-stack">
+            <Text className="text-xl"> • React Native</Text>
+            <Text className="text-xl"> • Expo</Text>
+            <Text className="text-xl"> • Nativewind</Text>
+            <Text className="text-xl"> • NativewindUI</Text>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
